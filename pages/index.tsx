17e413582a5fe6cd70a29cf435c1c6949e32a50b
Muhammad Maaz Uddin <maaznadeem246@gmail.com>
2022-07-20 13:27:13 +0500

    basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,16 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className="text-2xl text-red-200">
-      Hello world      
+    <div className="h-screen w-screen flex flex-col justify-center align-middle items-center">
+      <div className='text-2xl text-center'>
+          Which Pokemon is rounded
+      </div>
+      <div className='p-2' />
+      <div className='border rounded p-8 flex justify-between items-center max-w-2xl text-center  '>
+        <div className='w-16 h-16 bg-red-200'></div>
+        <div className='p-8'>Vs</div>
+        <div className='w-16 h-16 bg-red-200'></div>
+      </div>
     </div>
   )
 }
